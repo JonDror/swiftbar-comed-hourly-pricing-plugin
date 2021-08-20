08f380b58e35f553c79b8d4a25443185015df470
Jonathan Dror <jon@dror.io>
2021-08-20 09:48:42 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# comEd hourly pricing indicator
+Plugin for swiftbar the shows the current 5 min price per kWh using [comEd APIs](https://hourlypricing.comed.com/hp-api/).
+
+Price includes a SF Symbol indicator that will change colors depending on the price:
+* green when `< 5` 
+* orange when between `5`  and `10`
+* red when `>10`
+
+![](README/08D8CE1E-5959-4CA0-836B-F420CF389E90.png)
+![](README/17FF657F-A2E5-42AC-A578-9B6509149923.png)
+
---
+++ comed.30s.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+#encoding: utf-8
+
+# <xbar.title>comEd Current hourly price</xbar.title>
+# <xbar.version>v1.0</xbar.version>
+# <xbar.author>Jonathan Dror</xbar.author>
+# <xbar.author.github>jondror</xbar.author.github>
+# <xbar.desc>See the current price of comEd hourly pricing.</xbar.desc>
+# <xbar.dependencies>python, requests module</xbar.dependencies>
+
+import requests
+import datetime as dt
+import pytz
+import json
+
+def tz_from_utc_ms_ts(utc_ms_ts, tz_info):
+    """Given millisecond utc timestamp and a timezone return dateime
+
+    :param utc_ms_ts: Unix UTC timestamp in milliseconds
+    :param tz_info: timezone info
+    :return: timezone aware datetime
+    """
+    # convert from time stamp to datetime
+    utc_datetime = dt.datetime.utcfromtimestamp(utc_ms_ts / 1000.)
+
+    # set the timezone to UTC, and then convert to desired timezone
+    return utc_datetime.replace(tzinfo=pytz.timezone('UTC')).astimezone(tz_info)
+
+comedCurrentApi = "https://hourlypricing.comed.com/api?type=5minutefeed"
+url = requests.get(comedCurrentApi)
+text = url.text
+
+data = json.loads(text)
+
+currentPrice = data[0]
+priceUSD = currentPrice['price']
+
+
+## Decide on the color. default: orange
+alertColor = "orange"
+if float(priceUSD) < 5:
+	alertColor = "green"
+elif float(priceUSD) > 10:
+	alertColor = "red"
+
+priceTimeUTC = currentPrice['millisUTC']
+priceTimeInChicago = tz_from_utc_ms_ts(int(priceTimeUTC), pytz.timezone('America/Chicago'))
+# print(str(priceUSD)+"¢ :electric_plug: | symbolize=false color="+alertColor)
+print(str(priceUSD)+"¢ :gauge: | sfcolor="+alertColor)
+print ("---")
+print ("as of "+priceTimeInChicago.strftime('%H:%M%p'))
+print ("---")
+print("Original JSON: | href="+comedCurrentApi)
+print(data[0])
