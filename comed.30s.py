5e507ed0e21a075756b634a0ede173e221a1f96c
JonDror <jon@dror.io>
2021-08-20 20:40:40 -0500

    tweak fetch and added history

--- comed.30s.py
+++ comed.30s.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 #encoding: utf-8
 
-# <xbar.title>comEd Current hourly price</xbar.title>
-# <xbar.version>v1.0</xbar.version>
-# <xbar.author>Jonathan Dror</xbar.author>
-# <xbar.author.github>jondror</xbar.author.github>
-# <xbar.desc>See the current price of comEd hourly pricing.</xbar.desc>
-# <xbar.dependencies>python, requests module</xbar.dependencies>
+# <bitbar.title>comEd Current hourly price</xbar.title>
+# <bitbar.version>v1.0</xbar.version>
+# <bitbar.author>Jonathan Dror</xbar.author>
+# <bitbar.author.github>JonDror</xbar.author.github>
+# <bitbar.desc>See the current price of comEd hourly pricing.</xbar.desc>
+# <bitbar.dependencies>python, requests module</xbar.dependencies>
 
 import requests
 import datetime as dt
@@ -16,27 +16,24 @@ import json
 comedCurrentApi = "https://hourlypricing.comed.com/api?type=5minutefeed"
 tz = "America/Chicago"
 
-def tz_from_utc_ms_ts(utc_ms_ts, tz_info):
-    """Given millisecond utc timestamp and a timezone return dateime
-
-    :param utc_ms_ts: Unix UTC timestamp in milliseconds
-    :param tz_info: timezone info
-    :return: timezone aware datetime
-    """
+def utc_ms_to_timezone(utc_ms_ts):
     # convert from time stamp to datetime
-    utc_datetime = dt.datetime.utcfromtimestamp(utc_ms_ts / 1000.)
+    utc_datetime = dt.datetime.utcfromtimestamp(int(utc_ms_ts) / 1000.)
 
     # set the timezone to UTC, and then convert to desired timezone
-    return utc_datetime.replace(tzinfo=pytz.timezone('UTC')).astimezone(tz_info)
+    return utc_datetime.replace(tzinfo=pytz.timezone('UTC')).astimezone(pytz.timezone(tz)).strftime('%H:%M%p')
 
+# Get data
 url = requests.get(comedCurrentApi)
 text = url.text
-
 data = json.loads(text)
 
+# Get current price
 currentPrice = data[0]
 priceUSD = currentPrice['price']
 
+# Get current time
+priceTimeUTC = currentPrice['millisUTC']
 
 ## Decide on the color. default: orange
 alertColor = "orange"
@@ -45,12 +42,15 @@ if float(priceUSD) < 5:
 elif float(priceUSD) > 10:
 	alertColor = "red"
 
-priceTimeUTC = currentPrice['millisUTC']
-priceTimeInLocalTZ = tz_from_utc_ms_ts(int(priceTimeUTC), pytz.timezone(tz))
-# print(str(priceUSD)+"¢ :electric_plug: | symbolize=false color="+alertColor)
+## Print menu bar
 print(str(priceUSD)+"¢ :gauge: | sfcolor="+alertColor)
+
+## Print dropdown
 print ("---")
-print ("as of "+priceTimeInLocalTZ.strftime('%H:%M%p'))
+print ("updated at "+utc_ms_to_timezone(priceTimeUTC))
 print ("---")
-print("Original JSON: | href="+comedCurrentApi)
-print(data[0])
+print ("History:")
+for x in range(6):
+  print(utc_ms_to_timezone(data[x]['millisUTC'])+": "+data[x]['price']+"¢")
+print ("---")
+print("Click here to see original JSON | href="+comedCurrentApi)
